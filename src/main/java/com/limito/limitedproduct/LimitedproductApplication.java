32ed8c722a3f98c3e8ecd83952d5810f8d07b998
River is line <dmstjs7047@hanmail.net>
2025-12-05 17:12:18 +0900

    test : http client에 인당 구매 가능 수량 조회 실패 경우도 추가

--- src/main/java/com/limito/limitedproduct/LimitedproductApplication.java
+++ src/main/java/com/limito/limitedproduct/LimitedproductApplication.java
@@ -3,7 +3,7 @@ package com.limito.limitedproduct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.limito")
 public class LimitedproductApplication {
 
 	public static void main(String[] args) {
