f9d221c32f289c5182d68b77076067d6fd6c84c1
River is line <dmstjs7047@hanmail.net>
2025-12-09 02:22:14 +0900

    feat (#10) : item id가 같으면 같은 아이템이라는 도메인 정책을 ItemAmount에 구현함.

--- src/main/java/com/limito/limitedproduct/domain/vo/ItemAmount.java
+++ src/main/java/com/limito/limitedproduct/domain/vo/ItemAmount.java
@@ -1,5 +1,6 @@
 package com.limito.limitedproduct.domain.vo;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import lombok.Builder;
@@ -11,4 +12,23 @@ public class ItemAmount {
 
 	private UUID itemId;
 	private int amount;
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(itemId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+
+		ItemAmount other = (ItemAmount)obj;
+		return itemId.equals(other.itemId);
+	}
 }
