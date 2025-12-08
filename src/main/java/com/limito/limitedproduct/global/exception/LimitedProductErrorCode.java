60b1be5a47acc08a8006e87cbb9180101894e235
River is line <dmstjs7047@hanmail.net>
2025-12-08 12:08:49 +0900

    chore (#29) : error code의 Httpstatus를 의미가 더 잘 나타나도록 변경

--- src/main/java/com/limito/limitedproduct/global/exception/LimitedProductErrorCode.java
+++ src/main/java/com/limito/limitedproduct/global/exception/LimitedProductErrorCode.java
@@ -12,8 +12,8 @@ public enum LimitedProductErrorCode implements ErrorCode {
 	PRODUCT_WRONG_UUID(HttpStatus.NOT_FOUND, "잘못된 상품 id입니다."),
 	PRODUCT_OPTION_WRONG_UUID(HttpStatus.NOT_FOUND, "잘못된 상품 옵션 id입니다."),
 	PRODUCT_ITEM_WRONG_UUID(HttpStatus.NOT_FOUND, "잘못된 상품 아이템 id입니다."),
-	PRODUCT_WRONG_CATEGORY_ID(HttpStatus.BAD_REQUEST, "상품 카테고리가 올바르지 않습니다. 이미 등록된 상품입니다."),
-	PRODUCT_WRONG_BRAND_NAME(HttpStatus.BAD_REQUEST, "브랜드명이 올바르지 않습니다. 이미 등록된 상품입니다.");
+	PRODUCT_WRONG_CATEGORY_ID(HttpStatus.CONFLICT, "상품 카테고리가 올바르지 않습니다. 이미 등록된 상품입니다."),
+	PRODUCT_WRONG_BRAND_NAME(HttpStatus.CONFLICT, "브랜드명이 올바르지 않습니다. 이미 등록된 상품입니다.");
 
 	private final HttpStatus status;
 	private final String message;
