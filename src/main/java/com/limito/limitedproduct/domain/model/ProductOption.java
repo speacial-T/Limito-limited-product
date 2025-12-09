acc9947ab47ac55c24a10a5462d65c0f07f8635e
River is line <dmstjs7047@hanmail.net>
2025-12-09 16:20:29 +0900

    feat (#10) : 옵션 품절 상태였으면 재고 복원될 때, 품절 해제하는 로직 추가

--- src/main/java/com/limito/limitedproduct/domain/model/ProductOption.java
+++ src/main/java/com/limito/limitedproduct/domain/model/ProductOption.java
@@ -110,7 +110,7 @@ public class ProductOption {
 	}
 
 	public void makeItemSoldOut(UUID productItemId) {
-		//TODO: for-if 개선 필요
+		//TODO: refactor - for-if
 		// TODO: refactor - for-if
 		for (ProductItem productItem : itemList) {
 			if (productItem.getId().equals(productItemId)) {
@@ -123,7 +123,7 @@ public class ProductOption {
 	}
 
 	private void checkAllSoldOut() {
-		//TODO: for-if 개선 필요
+		//TODO: refactor - for-if
 		for (ProductItem productItem : itemList) {
 			if (!productItem.isSoldOut()) {
 				this.isSoldOut = false;
@@ -152,8 +152,14 @@ public class ProductOption {
 		for (ProductItem productItem : itemList) {
 			if (productItem.getId().equals(itemId)) {
 				productItem.rollbackStock(amount);
-				//TODO: ProductOption - soldout = true면 FALSe로 바꿔야함.
+				changeIsSoldOutToFalse();
 			}
+		}
+	}
+
+	private void changeIsSoldOutToFalse() {
+		if (isSoldOut) {
+			isSoldOut = false;
 		}
 	}
 }
