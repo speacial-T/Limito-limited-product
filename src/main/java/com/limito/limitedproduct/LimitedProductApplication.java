03dd7e42697cc55819a9eb76315f499e9381ccdb
River is line <dmstjs7047@hanmail.net>
2025-12-21 04:07:02 +0900

    config : common library version upgrade
    v1.1.2 -> v2.0.2

--- src/main/java/com/limito/limitedproduct/LimitedProductApplication.java
+++ src/main/java/com/limito/limitedproduct/LimitedProductApplication.java
@@ -1,9 +1,7 @@
 package com.limito.limitedproduct;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.limito")
 public class LimitedProductApplication {
 
 	public static void main(String[] args) {
