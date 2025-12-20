d6a3533dfbaf864f5e4dfa9a0e9491d7de0a316c
River is line <dmstjs7047@hanmail.net>
2025-12-21 02:15:28 +0900

    feat : findAlById를 findAllByIdSet으로 변경
    parameter를 List에서 Set으로 변경

--- src/main/java/com/limito/limitedproduct/infrastructure/persistence/repository/productoption/ProductItemRepositoryImpl.java
+++ src/main/java/com/limito/limitedproduct/infrastructure/persistence/repository/productoption/ProductItemRepositoryImpl.java
@@ -1,12 +1,11 @@
 package com.limito.limitedproduct.infrastructure.persistence.repository.productoption;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
 
-import com.limito.common.exception.AppException;
-import com.limito.limitedproduct.application.exception.LimitedProductErrorCode;
 import com.limito.limitedproduct.domain.repository.ProductItemRepository;
 import com.limito.limitedproduct.domain.vo.ProductItem;
 
@@ -19,17 +18,7 @@ public class ProductItemRepositoryImpl implements ProductItemRepository {
 	private final ProductItemJpaRepository productItemJpaRepository;
 
 	@Override
-	public List<ProductItem> findAllById(List<UUID> uuidList) {
-		List<ProductItem> productItemList = productItemJpaRepository.findAllById(uuidList);
-
-		validateFindAllById(uuidList, productItemList);
-
-		return productItemList;
-	}
-
-	private void validateFindAllById(List<UUID> request, List<ProductItem> response) {
-		if (request.size() != response.size()) {
-			throw AppException.of(LimitedProductErrorCode.PRODUCT_ITEM_WRONG_UUID);
-		}
+	public List<ProductItem> findAllByIdSet(Set<UUID> itemIdSet) {
+		return productItemJpaRepository.findAllById(itemIdSet);
 	}
 }
